90147466eba0d28ea660bff463071b27bd830ba8
Mvro-24 <marwanmohamwd099@gmail.com>
2026-03-16 10:15:05 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let btn = document.getElementById("btn-light-dark")
-
+let btn3 = document.getElementById("btn-light-dark2")
 
 
 btn.onclick = function(){
@@ -11,6 +11,16 @@ btn.onclick = function(){
 
     else(btn.innerHTML ='<i class="fa-solid fa-moon"></i>')
 }
+btn3.onclick = function(){
+
+    document.body.classList.toggle("dark")
+
+    if (document.body.classList.contains("dark")){
+        btn.innerHTML=' <i class="fa-regular fa-sun"></i>'}
+
+    else(btn.innerHTML ='<i class="fa-solid fa-moon"></i>')
+}
+
 
 let alertbt =  document.getElementById('alert-btn')
 let btn2 = document.getElementById('ok')
@@ -24,3 +34,20 @@ btn2.onclick = ()=> {blurs.classList.add('hide');document.body.classList.remove(
 
 
 
+let bar = document.getElementById("bars")
+let ph = document.querySelector('.nav-ph')
+let nav_com = document.getElementById('nav-com')
+let close = document.getElementById('close')
+bar.onclick=function(){
+    ph.classList.remove('hide')
+    document.body.classList.add('no-scroll')
+    nav_com.classList.add('hide')
+    btn.classList.remove('hide')
+}
+
+close.onclick=function(){
+    ph.classList.add('hide')
+    document.body.classList.remove('no-scroll')
+    nav_com.classList.remove('hide')
+    
+}
